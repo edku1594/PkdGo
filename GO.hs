c853a8464c38dc013e3a90a5273d1b0785e0706a
MarieKP95 <mariepaulsson95@hotmail.com>
2016-02-22 16:26:54 +0100

    Nu fixat initialBoard (den kom inte med tidigare) och getCell

--- GO.hs
+++ GO.hs
@@ -15,6 +15,26 @@ data Cell = Empty | Stone deriving (Show)
 type Playfield = [[Cell]]
 type Pos = (Int,Int)
 
+-- instance Show Stone where     
+-- show Black = "X"					( Inget att tänka på nu )
+-- show White = "O"
+
+-- initialBoard makes a nxn board
+initialBoard :: Int -> Cell -> Playfield
+initialBoard n cell = replicate n (replicate n cell)
+
+-- kollar om cellen är tom?
+-- isEmpty :: Playfield -> Pos -> Bool
+-- isEmpty board (kolumn,rad) = Empty == (getCell board (kolumn,rad)) -- Den klagar på "=="
+
+-- tar fram vad som finns i den positionen
+getCell :: Playfield -> Pos -> Cell
+getCell board (kolumn,rad) = board!!kolumn!!rad
+
+-- playStone ska sätta ut en sten (ändrar Empty till Stone på den pos)
+-- playStone :: Pos -> Cell -> Cell
+
+--}
 
 {- Ex.2
 type Kolumn = Int
@@ -26,4 +46,7 @@ type Move = (Kolumn,Rad) -- (Int,Int) som spelarens input
 -- readMove :: IO Move -- reads input from player (?)
 -}
 
----https://github.com/haskelllive/haskelllive/blob/episode-1/Chess.hs
+-- ********** Hemsidor **************
+-- https://github.com/haskelllive/haskelllive/blob/episode-1/Chess.hs
+-- https://github.com/steshaw/hsChess
+-- https://github.com/mcmaniac/Go
