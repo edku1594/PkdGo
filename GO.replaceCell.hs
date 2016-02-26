c54279efe41a38b529ea52a7bf59a44ccc4f0791
Skedis <Skedis@Edas-MacBook-Pro.local>
2016-02-26 12:20:18 +0100

    dagbok uppp.

---
+++ GO.replaceCell.hs
@@ -0,0 +1,89 @@
+import Control.Exception
+import Prelude hiding(catch)
+import Control.Monad
+import Test.HUnit -- test framework
+
+import Data.Array
+import Data.List
+
+-----------------------------------------------------
+-- Ex.1
+-- ****************** data types ********************
+data Stone = Black | White deriving (Eq, Show)
+data Cell = Empty | Stone deriving (Eq, Show)
+data Player = A | B
+
+type Playfield = [[Cell]]
+type Pos = (Int,Int)
+
+-- instance Show Stone where     
+-- show Black = "X"					( Inget att tänka på nu )
+-- show White = "O"
+
+{--
+main :: IO ()
+main = do 
+  putStrLn "Welcome to Go."					( Från Nim.hs )
+  gameState <- genGameState 
+  play gameState
+  
+putStrLn "Play again? yes / no"
+continue <- getLine
+when (continue == "yes") $ do main
+--}
+
+
+-- initialBoard makes a nxn board (n lists with n elements each within a list)
+initialBoard :: Int -> Cell -> Playfield
+initialBoard n cell = replicate n (replicate n cell) -- blir bara en lång rad (inte kolumner å rader)
+
+-- Opposite of Stone and Empty
+oppositeCell :: Cell -> Cell
+oppositeCell Stone = Empty
+oppositeCell Empty = Stone
+
+
+-- kollar om cellen är Empty
+isEmpty :: Playfield -> Pos -> Bool
+isEmpty board (kolumn,rad) = if (getCell board (kolumn,rad)) == Empty then True else False 
+-- Ex: isEmpty [[Empty,Empty],[Stone,Empty]] (1,0) -> False
+--	   isEmpty [[Empty,Empty],[Stone,Empty]] (1,1) -> True
+
+-- tar fram vad som finns i den positionen
+getCell :: Playfield -> Pos -> Cell
+getCell board (kolumn,rad) = board!!kolumn!!rad -- (kolumn = vilken lista, rad = n:te elementet i listan)
+-- Ex: getCell [[Empty,Empty],[Empty,Empty],[Stone, Empty]] (2,1) -> Empty
+--     getCell [[Empty,Empty],[Empty,Empty],[Stone, Empty]] (2,0) -> Stone
+
+
+-- playStone ska sätta ut en sten (ändrar Empty till Stone på den pos)
+-- playStone :: Playfield -> Pos -> [Cell] -> Playfield
+-- playStone board (kolumn,rad) sten = if getCell board (kolumn,rad) == Empty then (replaceCell board (kolumn,rad) sten) else board
+
+-- replaceCell byter ut ett element på Pos 
+-- replaceCell :: [a] -> Pos -> a -> [a]
+replaceCell board (xlist,yelem) c  = replaceCell' board xlist (replaceCell' (board!!xlist) yelem c)
+
+-- replaceCell' (auxiliary funktion) gör allt jobb åt replaceCell
+replaceCell' :: [a] -> Int -> a -> [a]
+replaceCell' board i c = 
+	if and [i >= 0, i < length board]		--getCell board (lista,elementet) == Empty måste vara med!
+	then take i board ++ (c: (drop (i+1) board))
+	else board
+
+
+{- Ex.2
+type Kolumn = Int
+type Rad = Int
+type Move = (Kolumn,Rad) -- (Int,Int) som spelarens input
+
+-----type Player = String
+
+-- readMove :: IO Move -- reads input from player (?)
+-}
+{-
+********** Hemsidor **************
+https://github.com/haskelllive/haskelllive/blob/episode-1/Chess.hs
+https://github.com/steshaw/hsChess
+https://github.com/mcmaniac/Go
+-}
